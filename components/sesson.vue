df8d8b4f135af316bb46885a706dc54eb4b9e94e
esamani77 <esamani77@gmail.com>
2022-03-23 01:57:16 +0430

    add dynmic formula

--- components/sesson.vue
+++ components/sesson.vue
@@ -1,46 +1,26 @@
 <template>
   <div class="bg-gray-100 rounded-xl p-8">
     <p class="text-lg font-semibold text-right">
-      {{ store.lessons[index].name }}
+      {{ inputs }}
     </p>
   </div>
   <div class="text-right m-5" dir="rtl">
     <div
       class="border-gray-500"
-      v-for="formula in store.lessons[index].formulas"
+      v-for="(formula, formulaIndex) in store.lessons[index].formulas"
       :key="formula.name"
     >
       <h4>
         {{ formula.name }}
       </h4>
-      <div class="mt-5" v-for="(param, idx) in formula.params[0]" :key="param">
-        <label :for="param"> {{ idx }} :</label>
+      <div class="mt-5" v-for="(param, idx) in inputs[formulaIndex]" :key="idx">
+        <label :for="param.key">{{ param.key }}</label>
         <input
-          :id="param"
-          type="text"
-          class="
-            mt-1
-            block
-            w-full
-            px-3
-            py-2
-            bg-white
-            border border-slate-300
-            rounded-md
-            text-sm
-            shadow-sm
-            placeholder-slate-400
-            focus:outline-none
-            focus:border-sky-500
-            focus:ring-1
-            focus:ring-sky-500
-            disabled:bg-slate-50
-            disabled:text-slate-500
-            disabled:border-slate-200
-            disabled:shadow-none
-            invalid:border-pink-500 invalid:text-pink-600
-            focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-          "
+          type="number"
+          :name="param.key"
+          :id="param.key"
+          v-model="param.value"
+          class="border border-gray-300 bg-orange-200"
         />
       </div>
     </div>
@@ -58,6 +38,38 @@ export default {
   computed: {
     index() {
       return parseFloat(this.$route.params.id - 1) || 0;
+    },
+    inputs() {
+      let data = this.store.lessons[this.index].formulas,
+        inputs = [];
+      console.log(data);
+      data.forEach((el, foreachIndex) => {
+        console.log(foreachIndex, el.formula);
+        let specialFormula = el.formula,
+          regex = new RegExp(/var/gm);
+        inputs.push({});
+        for (let idx in specialFormula) {
+          let index = parseInt(idx);
+          let word =
+            specialFormula[index] +
+            specialFormula[index + 1] +
+            specialFormula[index + 2];
+
+          if (regex.test(word)) {
+            let key = word + specialFormula[index + 3];
+            inputs[foreachIndex] = { ...inputs[foreachIndex], [key]: "" };
+          }
+        }
+      });
+      console.log(inputs);
+      let formuls = [];
+      inputs.forEach((el, idx) => {
+        formuls.push([]);
+        for (let key in el) {
+          formuls[idx].push({ key: key, value: "" });
+        }
+      });
+      return formuls;
     },
   },
 };
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,21 @@ import { defineNuxtConfig } from "nuxt3";
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: ["@/assets/css/main.css", "@/assets/css/estedad.css"],
+  head: {
+    script: [
+      {
+        type: "text/javascript",
+        src: "https://polyfill.io/v3/polyfill.min.js?features=es6",
+        id: "",
+      },
+      {
+        type: "text/javascript",
+        src: "https://cdn.jsdelivr.net/npm/mathjax@3/es5/mml-chtml.js",
+        id: "MathJax-script",
+        async: true,
+      },
+    ],
+  },
   build: {
     postcss: {
       postcssOptions: require("./postcss.config.js"),
